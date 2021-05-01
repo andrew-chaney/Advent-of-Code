ae2d05c9b20f2086b50bb2e5d28440dcf92c54e1
andrew-chaney <60203781+andrew-chaney@users.noreply.github.com>
2021-04-30 21:01:33 -0400

    Update main.py

--- 2015/Day_24/main.py
+++ 2015/Day_24/main.py
@@ -13,27 +13,15 @@ def findMinQE(packages: list[int], groups: int) -> int:
     return min([prod(i) for i in possible])
 
 def main():
-    base = "/Users/andrewchaney/Documents/Coding Challenges/Advent-of-Code/2015/Day_24/"
-    path = base + "puzzle_input.txt"
+    path = "puzzle_input.txt"
 
     with open(path, "r") as file:
         packages = [int(line) for line in file]
-
-    test = [1,2,3,4,5,7,8,9,10,11]
-
-    # for n in range(len(packages)):
-    #     possible = [i for i in combinations(packages, n) if sum(i) == sum(packages) / 3]
-    #     if len(possible) > 0:
-    #         break
     
-    # print(min([prod(i) for i in possible]))
-
     part1 = findMinQE(packages, 3)
     part2 = findMinQE(packages, 4)
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}")
-
-                    
 
 if __name__ == "__main__":
     main()
