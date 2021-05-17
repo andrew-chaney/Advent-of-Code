6f601aba26dbbc8b2f043e67ad9931847871a253
Andrew Chaney <andrewchaney24@gmail.com>
2021-05-17 13:29:34 -0400

    Updated day1.java

--- 2016/Day_1/day1.java
+++ 2016/Day_1/day1.java
@@ -3,42 +3,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.lang.Math;
 
-class Coordinates {
-    private int x;
-    private int y;
-
-    public Coordinates(int newX, int newY) {
-        x = newX;
-        y = newY;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void incX(int i) {
-        x += i;
-    }
-
-    public void decX(int i) {
-        x -= i;
-    }
-
-    public void incY(int i) {
-        y += i;
-    }
-
-    public void decY(int i) {
-        y -= i;
-    }
-}
-
 public class day1 {
-
     public static void main (String[] argv) {
         String path = "puzzle_input.txt";
         File myFile = new File(path);
