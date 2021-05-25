0fd768ff42dee7453e52f8357f0973ec738eca73
Andrew Chaney <andrewchaney24@gmail.com>
2021-05-25 10:10:55 -0400

    Updated day11.java

--- 2016/Day_11/day11.java
+++ 2016/Day_11/day11.java
@@ -28,7 +28,8 @@ public class day11 {
             System.exit(1);
         }
         /*
-            The amount of moves to move items up a floor based on the number of items on the current floor (remember, elevator capacity is 2):
+            The amount of moves to move items up a floor based on the number of 
+            items on the current floor (remember, elevator capacity is 2):
                 1 item = 1 move   (up with 2)
                 2 items = 1 move  (up with 2)
                 3 items = 3 moves (up with 2, down with 1, up with 2)
@@ -39,26 +40,44 @@ public class day11 {
                 8 items = 13 moves
                   ...   =   ...
 
-            So, let's say that y represents the number of moves required to move all items on a floor up to the next floor and that x represents the number of items that need to be moved.
+            So, let's say that y represents the number of moves required to 
+            move all items on a floor up to the next floor and that x 
+            represents the number of items that need to be moved.
 
-            We know that x = 1, only 1 item on the floor that needs to be shifted, is an anomoly. Optimally, we will always move 2 items in one trip wherever we can move 2 items at once. Therefor, when y = x when x <= 1.
+            We know that x = 1, only 1 item on the floor that needs to be 
+            shifted, is an anomoly. Optimally, we will always move 2 items in 
+            one trip wherever we can move 2 items at once. Therefor, when y = x 
+            when x <= 1.
 
-            So, we can say that for every x where x > 1 the amount of moves is divided by 2 since we are taking 2 per trip. But, we have to also keep in mind that we always have to have at least one item in the elevator as well, so we'll need a constant.
+            So, we can say that for every x where x > 1 the amount of moves is 
+            divided by 2 since we are taking 2 per trip. But, we have to also 
+            keep in mind that we always have to have at least one item in the 
+            elevator as well, so we'll need a constant.
                 Symbolically: 
                     y / 2 = x
                     So, y = 2x + C
-            When we set x = 2 we get y = 2(2) + C. We already know the moves necessary to move 2 items up a floor is y = 1. So, 1 = 4 + C and we get that C = 3. This makes sense, because there will be a trip where you don't have to go up, down, and back up in the  elevator for each floor.
+            When we set x = 2 we get y = 2(2) + C. We already know the moves 
+            necessary to move 2 items up a floor is y = 1. So, 1 = 4 + C and we 
+            get that C = 3. This makes sense, because there will be a trip 
+            where you don't have to go up, down, and back up in the  elevator 
+            for each floor.
             
-            To test our new equation of y = 2x - 3 we can just compare it to the table to confirm that all of its values match.
+            To test our new equation of y = 2x - 3 we can just compare it to 
+            the table to confirm that all of its values match.
 
             All said and done we get the following equation:
                     { y = 1         for x < 2
                 y = { Note, items will only ever be whole numbers.
                     { y = 2x - 3    for 2 <= x < infiniti
 
-            NOTE: This does NOT work with the test or other various inputs because this equation completely ignores the actual items themselves. It doesn't match generators and microchips properly, as required by some instances of the problem. 
+            NOTE: This does NOT work with the test or other various inputs 
+            because this equation completely ignores the actual items 
+            themselves. It doesn't match generators and microchips properly, as 
+            required by some instances of the problem. 
 
-            When I tested this against my input it worked. I may come back to solve the problem in a more intentional way eventually, but with how busy I am currently this will suffice.
+            When I tested this against my input it worked. I may come back to 
+            solve the problem in a more intentional way eventually, but with 
+            how busy I am currently this will suffice.
         */
         System.out.println("Part 1: " + move(input, false) + " Steps");
         System.out.println("Part 2: " + move(input, true) + " Steps");
