f1c8e4fb8644826fb36231c7a0f233ed6fa16524
Andrew Chaney <andrewchaney24@gmail.com>
2021-10-24 14:37:52 -0600

    2017 Day 5 Solution

---
+++ 2017/Day_5/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int parseJumps(vector<int> input, bool part2) {
+    int steps = 0;
+    int index = 0;
+    while (true) {
+        if (index >= input.size())
+        {
+            return steps;
+        }
+        int mod = 0;
+        if (part2 == false) {
+            mod = 1;
+        } else {
+            if (input[index] >= 3) {
+                mod = -1;
+            }
+            else {
+                mod = 1;
+            }
+        }
+        input[index] += mod;
+        index += input[index] - mod;
+        steps++;
+    }
+}
+
+int main() {
+    string path = "puzzle_input.txt";
+    fstream file;
+
+    vector<int> input;
+
+    file.open(path, ios::in);
+    if (!file) {
+        cout << "Can't access the file " << path << endl;
+        return 1;
+    } else {
+        string line;
+        while (getline(file, line)) {
+            int num = stoi(line);
+            input.push_back(num);
+        }
+    }
+
+    cout << parseJumps(input, false) << endl;
+    cout << parseJumps(input, true) << endl;
+
+    return 0;
+}
